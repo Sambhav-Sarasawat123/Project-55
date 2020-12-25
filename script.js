f841604f5ca97a666c524b526b90429e69eb838f
Sambhav-Sarasawat123 <76476750+Sambhav-Sarasawat123@users.noreply.github.com>
2020-12-25 13:19:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// script
+function a() {
+    alert('The Great Wall of China is an ancient wall in China. The wall is made of cement, rocks, bricks, and powdered dirt. It was finished in 1878 and it was meant to protect the north of the empire of China from enemy attacks. It is the longest structure humans have ever built.');
+}
+function b() {
+    alert('Chichen Itza was a large pre-Columbian city built by the Maya people of the Terminal Classic period. The archaeological site is located in Tinúm Municipality, Yucatán State, Mexico. Chichen Itza was a major focal point in the Northern Maya Lowlands from the Late Classic through the Terminal Classic and into the early portion of the Postclassic period.');
+}
+function c() {
+    alert('Petra (Arabic: ٱلْبَتْرَاء‎, romanized: Al-Batrāʾ; Ancient Greek: Πέτρα, "Rock"), originally known to its inhabitants in as Raqmu or Raqēmō, is a historic and archaeological city in southern Jordan.');
+}
+function d() {
+    alert('Machu Picchu is a 15th-century Inca citadel, located in the Eastern Cordillera of southern Peru, on a 2,430-metre mountain ridge. It is located in the Machupicchu District within Urubamba Province above the Sacred Valley, which is 80 kilometres northwest of Cuzco.');
+}
+function e() {
+    alert('Christ the Redeemer is an Art Deco statue of Jesus Christ in Rio De Janeiro, Brazil, created by Polish-French sculptor Paul Landowski and built by Brazilian engineer Heitor da Silva Costa, in collaboration with French engineer Albert Caquot. Romanian sculptor Gheorghe Leonida fashioned the face. Constructed between 1922 and 1931, the statue is 30 metres high, excluding its 8-metre pedestal. The arms stretch 28 metres wide.');
+}
+function f() {
+    alert('The Colosseum , is an oval amphitheatre in the centre of the city of Rome, Italy, just east of the Roman Forum and is the largest ancient amphitheatre ever built, and is still the largest standing amphitheater in the world today, despite its age.');
+}
+function g() {
+    alert('The Taj Mahal is an ivory-white marble mausoleum on the southern bank of the river Yamuna in the Indian city of Agra. It was commissioned in 1632 by the Mughal emperor Shah Jahan (reigned from 1628 to 1658) to house the tomb of his favourite wife, Mumtaz Mahal; it also houses the tomb of Shah Jahan himself. The tomb is the centrepiece of a 17-hectare (42-acre) complex, which includes a mosque and a guest house, and is set in formal gardens bounded on three sides by a crenellated wall. ');
+}
